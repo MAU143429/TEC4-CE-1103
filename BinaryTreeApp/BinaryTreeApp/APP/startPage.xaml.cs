a8a52ddd09648f1093a5392578fb6de98a307a8e
MAU <mau141226@gmail.com>
2020-07-28 03:16:29 -0600

    [Add] IBinary Tree Interface was added

---
+++ BinaryTreeApp/BinaryTreeApp/APP/startPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BinaryTreeApp.APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class startPage : ContentPage
+    {
+        public startPage()
+        {
+            InitializeComponent();
+        }
+
+        public void main_app(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new mainPage());
+        }
+    }
+}
